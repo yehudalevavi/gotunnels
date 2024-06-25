5385730b1d8f3b39a17cf1cbf140f7f54f62c96f
Yehuda Levavi <ylevavi@tenable.com>
2024-06-25 15:20:35 +0300

    add "GET" to router handlers

--- router/router.go
+++ router/router.go
@@ -6,11 +6,11 @@ import (
 )
 
 func DefaultRouter() {
-	http.HandleFunc("/moshe", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("GET /moshe", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("sending Moshe to client")
 		_, _ = w.Write([]byte("Moshe!\n"))
 	})
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("sending Hello World to client")
 		_, _ = w.Write([]byte("Hello World!\n"))
 	})
